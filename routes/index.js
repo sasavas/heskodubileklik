01770dc439764734c362fdc04237cce46bd19a9f
fsavas <savasusame@gmail.com>
2021-03-25 13:52:27 +0300

    add payment pages

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/purchase', function(req, res) {
-  res.render('purchase')
+router.get('/hakkimizda', function(req, res) {
+  res.render('hakkimizda')
+})
+
+router.get('/checkout/purchase', function(req, res) {
+  res.render('checkout/purchase')
+})
+
+router.get('/checkout/address', function(req, res) {
+  res.render('checkout/address')
 })
 
 router.get('/pages/gizlilikveguvenlik', function(req, res) {
@@ -18,9 +27,11 @@ router.get('/pages/mesafelisatissozlesmesi', function(req, res) {
   res.render('pages/mesafelisatissozlesmesi')
 })
 
-router.get('/hakkimizda', function(req, res) {
-  res.render('hakkimizda')
+router.post('/checkout/enteraddress', function (req, res) {
+  console.log('post method received by enter address post router')
+  res.redirect('checkout/purchase')
 })
+
 
 
 module.exports = router;
