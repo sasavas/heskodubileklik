1141417185255b5feeb9a3c18d4f0cc4d713729b
fsavas <savasusame@gmail.com>
2021-03-29 17:13:47 +0300

    fix form validations

--- public/javascripts/address.js
+++ public/javascripts/address.js
@@ -4,7 +4,6 @@
 
 toggleRequiredForTesting(false);
 
-
 function toggleRequiredForTesting(isRequired) {
   let inputs = document.querySelectorAll("input");
   let select = document.querySelectorAll("select");
@@ -22,6 +21,7 @@ function toggleRequiredForTesting(isRequired) {
 */
 
 let form = document.querySelector("form");
+let file = document.getElementById("hesqrcode");
 let citiesSelect = document.getElementById("cities");
 let townsSelect = document.getElementById("towns");
 let phone = document.getElementById("phone");
@@ -29,7 +29,6 @@ let email = document.getElementById("email");
 let email2 = document.getElementById("email2");
 let addr = document.getElementById("addressline1");
 let error = document.getElementById("error");
-
 
 /*
   Form data population
@@ -110,7 +109,6 @@ function createDefaultTownOption() {
   townsSelect.appendChild(initialTownOption);
 }
 
-
 /*
   Cleave validations
  */
@@ -119,7 +117,6 @@ var cleave = new Cleave(".input-phone", {
   phoneRegionCode: "TR",
 });
 
-
 /*
   Custom form validations
  */
@@ -127,87 +124,92 @@ var cleave = new Cleave(".input-phone", {
 var errors = [];
 
 form.addEventListener("submit", function (e) {
+  const fileError = "QR kodunu içeren bir dosya seçmelisin.";
   const cityError = "Şehir seçmen gerekiyor.";
-  const townError = "İlçe seçmen gerekiyor."
-  const addrError = "Daha uzun bir adres girmelisin."
-  const phoneError = "Telefon numaranı 10 haneli olarak girmelisin."
-  const emailError = "E-posta adresin yanlış"
-  const emailMatchError = "E-posta adreslerin eşleşmiyor, kontrol edip tekrar dene!";
+  const townError = "İlçe seçmen gerekiyor.";
+  const addrError = "Daha uzun bir adres girmelisin.";
+  const phoneError = "Telefon numaranı 10 haneli olarak girmelisin.";
+  const emailError = "E-posta adresin yanlış";
+  const emailMatchError =
+    "E-posta adreslerin eşleşmiyor, kontrol edip tekrar dene!";
 
   removeLeadingZeroOffPhone();
 
   clearErrorList();
-  
+
+  if (file.files.length === 0) {
+    dangerify(file)
+    addError(fileError)
+  }
+
   if (citiesSelect.selectedIndex === 0) {
-    e.preventDefault();
-    dangerify(citiesSelect)
-    addError(cityError)
+    dangerify(citiesSelect);
+    addError(cityError);
   }
 
-  if(townsSelect.selectedIndex <= 0) {
-    e.preventDefault();
-    dangerify(townsSelect)
-    addError(townError)
+  if (townsSelect.selectedIndex <= 0) {
+    dangerify(townsSelect);
+    addError(townError);
   }
 
-  if(addr.value.trim() < 20){
-    e.preventDefault()
-    dangerify(addr)
-    addError(addrError)
+  if (addr.value.trim() < 20) {
+    dangerify(addr);
+    addError(addrError);
   }
 
-  if(phone.value.length < 13){
-    e.preventDefault()
-    dangerify(phone)
-    addError(phoneError)
+  if (phone.value.length < 13) {
+    dangerify(phone);
+    addError(phoneError);
   }
 
-  if(email.value.trim() === '' || email.value === null){
-    e.preventDefault()
-    dangerify(email)
-    dangerify(email2)
+  if (email.value.trim() === "" || email.value === null) {
+    dangerify(email);
+    dangerify(email2);
 
-   addError(emailError)
+    addError(emailError);
   }
 
   if (email.value !== email2.value) {
+    dangerify(email);
+    dangerify(email2);
+    addError(emailMatchError);
+  }
+
+  if (errors.length > 0) {
     e.preventDefault();
-    dangerify(email)
-    dangerify(email2)
-    addError(emailMatchError)
   }
 
   showErrorList(errors);
 });
 
-function clearErrorList(){
-  errors = []
+function clearErrorList() {
+  errors = [];
 }
 
-function addError(error){
-  if(errors.indexOf(error) === -1 ){
+function addError(error) {
+  if (errors.indexOf(error) === -1) {
     errors.push(error);
   }
 }
 
-function dangerify(element){
-  element.style.border = "0px"
+function dangerify(element) {
+  element.style.border = "0px";
   element.style.outline = "1px solid red";
 }
 
-function normalify(element){
-  element.style.border = "1px solid black"
+function normalify(element) {
+  element.style.border = "1px solid black";
   element.style.outline = "0px";
 }
 
 function showErrorList(errors) {
   let errorList = document.getElementById("error-list");
   errorList.innerHTML = "";
-  
+
   if (errors.length === 0) {
     return;
   }
-  
+
   for (let i = 0; i < errors.length; i++) {
     let err = document.createElement("li");
     err.innerText = errors[i];
@@ -218,45 +220,55 @@ function showErrorList(errors) {
   // }, 10000);
 }
 
-citiesSelect.addEventListener('change', function(e) {
-  if(citiesSelect.selectedIndex > 0){
+file.addEventListener('change', function(e){
+  if(file.files.length > 0){
+    normalify(file)
+    // TODO: check if file contains a valid QR hes code
+  }
+})
+
+citiesSelect.addEventListener("change", function (e) {
+  if (citiesSelect.selectedIndex > 0) {
     normalify(citiesSelect);
   }
-})
+});
 
-townsSelect.addEventListener('change', function(e){
-  if(townsSelect.selectedIndex > 0){
+townsSelect.addEventListener("change", function (e) {
+  if (townsSelect.selectedIndex > 0) {
     normalify(townsSelect);
   }
-})
+});
 
-addr.addEventListener('input', function(e) {
-  if(addr.value.trim().length > 20){
-    normalify(addr)
+addr.addEventListener("input", function (e) {
+  if (addr.value.trim().length > 20) {
+    normalify(addr);
   }
-})
+});
 
-email.addEventListener('input', function(e){
-  if(email.value.trim().length > 10 && email.value.trim().indexOf("@") > -1){
-    normalify(email)
+email.addEventListener("input", function (e) {
+  if (email.value.trim().length > 10 && email.value.trim().indexOf("@") > -1) {
+    normalify(email);
   }
-})
+});
 
-email2.addEventListener('input', function(e){
-  if(email2.value.trim().length > 10 && email2.value.trim().indexOf("@") > -1){
-    normalify(email2)
+email2.addEventListener("input", function (e) {
+  if (
+    email2.value.trim().length > 10 &&
+    email2.value.trim().indexOf("@") > -1
+  ) {
+    normalify(email2);
   }
-})
+});
 
-phone.addEventListener('input', function(e){
-  removeLeadingZeroOffPhone()
-  if(phone.value.trim().length === 13){
-    normalify(phone)
+phone.addEventListener("input", function (e) {
+  removeLeadingZeroOffPhone();
+  if (phone.value.trim().length === 13) {
+    normalify(phone);
   }
-})
+});
 
-function removeLeadingZeroOffPhone(){
-  if(phone.value.substr(0,1) == 0){
-    phone.value = phone.value.substr(1)
+function removeLeadingZeroOffPhone() {
+  if (phone.value.substr(0, 1) == 0) {
+    phone.value = phone.value.substr(1);
   }
 }
